c7f50167518803866126b9433e123fd282e790c0
duckyduckG <duckyduckg65@gmail.com>
2026-04-05 00:50:06 +0500

    mm-video-v4l2: vidc: use sdm845 specific vidc_utils header
    Signed-off-by: duckyduckG <duckyduckg65@gmail.com>

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -34,7 +34,11 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <fcntl.h>
 #include "video_encoder_device_v4l2.h"
 #include "omx_video_encoder.h"
+#ifdef VENUS_SDM845_HEADER
+#include "media/msm_vidc_utils_sdm845.h"
+#else
 #include "media/msm_vidc_utils.h"
+#endif
 #ifdef USE_ION
 #include <linux/msm_ion.h>
 #endif
